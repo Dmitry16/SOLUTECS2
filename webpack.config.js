af4dfaadd45e0e114d12f616132e6a5822db3879
dd <dd@u16.12>
2017-04-20 14:20:00 +0200

    uglifyJSPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+const webpack = require('webpack')
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
   devtool: 'inline-sourcemap',
   entry: './src/index.js',
-
   output: {
     publicPath: '/',
     filename: 'bundle.js'
   },
-
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   module: {
     loaders: [
       {
